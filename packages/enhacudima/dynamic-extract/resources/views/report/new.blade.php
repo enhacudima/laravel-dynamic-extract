a666bcc0c7513aebba2515d5a4019d12e25f663f
Emídio Carlos Nhacudima <kalibredj@outlook.com>
2022-01-22 17:37:50 +0200

    fix footer

--- packages/enhacudima/dynamic-extract/resources/views/report/new.blade.php
+++ packages/enhacudima/dynamic-extract/resources/views/report/new.blade.php
@@ -10,14 +10,16 @@
     <div class="row">
     @foreach($data as $report)
         @if(config('dynamic-extract.auth') ? Auth::user()->can($report->can) : true)
-        <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">{{$report->name}}</h5>
-            <p class="card-text">{{$report->comments}}.</p>
-            <div class="text-end">
-                <a href="{{url('report/config/open',$report->id)}}" class="btn btn-primary btn-sm"><i class="fas fa-cloud-download-alt"></i></a>
+        <div class="col-sm-4">
+            <div class="card text-white bg-dark">
+                <div class="card-body">
+                    <h6 class="card-title">{{$report->name}}</h6>
+                    <p class="card-text"><small>{{$report->comments}}.</small></p>
+                    <div class="text-end">
+                        <a href="{{url('report/config/open',$report->id)}}" class="btn btn-primary btn-sm"><i class="fas fa-cloud-download-alt"></i></a>
+                    </div>
+                </div>
             </div>
-        </div>
         </div>
         @endif
     @endforeach
--- packages/enhacudima/dynamic-extract/resources/views/vendor/layouts/footer.blade.php
+++ packages/enhacudima/dynamic-extract/resources/views/vendor/layouts/footer.blade.php
@@ -1,5 +1,8 @@
-<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div class="col-md-4 d-flex align-items-center">
+<footer class="d-flex flex-wrap justify-content-between align-items-center border-top fixed-bottom">
+    <div class="d-flex ml-10 ms-3 align-items-center">
       <span class="text-muted">© 2021 Enhacudima</span>
     </div>
+    <ul class="nav col-md-4 justify-content-end list-unstyled ml-10">
+      <li class="ms-3 me-3"><a class="text-muted" href="https://github.com/enhacudima" target="_blank"><i class="fab fa-github"></i></a></li>
+    </ul>
 </footer>
